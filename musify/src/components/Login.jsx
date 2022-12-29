2f012f15eb0fd038dc404808ebda9d35a794b71e
LinuxDecoded <ajshrmaofficial@gmail.com>
2022-12-29 21:45:24 +0530

    added a permission

--- musify/src/components/Login.jsx
+++ musify/src/components/Login.jsx
@@ -3,7 +3,7 @@ import '../styles/App.css'
 import logo from '../assets/logo.svg'
 
 
-const AuthUrl = 'https://accounts.spotify.com/authorize?client_id=27b1cecea7ef4b93a30575c8f5c997d6&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+const AuthUrl = 'https://accounts.spotify.com/authorize?client_id=27b1cecea7ef4b93a30575c8f5c997d6&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read'
 
 const Login = () => {
     return(
