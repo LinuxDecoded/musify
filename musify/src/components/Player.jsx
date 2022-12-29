53b0188fa4c154272bd275975695ccb4709f2bcc
LinuxDecoded <ajshrmaofficial@gmail.com>
2022-12-29 22:54:00 +0530

    fixed lyrics button

--- musify/src/components/Player.jsx
+++ musify/src/components/Player.jsx
@@ -2,19 +2,23 @@ import { useState, useEffect } from "react"
 import SpotifyPlayer from "react-spotify-web-playback"
 import '../styles/App.css'
 
-const Player = ({ accessToken, trackUri }) => {
+const Player = ({ accessToken, trackUri, playingState }) => {
   const [play, setPlay] = useState(false)
-
+  const [isPlaying, setIsPlaying] = useState(false)
+  playingState(isPlaying)
   useEffect(() => setPlay(true), [trackUri])
   if (!accessToken) return null
   return (
     <>
-    {(trackUri && play==true) && <p>Playing...</p>}
+    {(trackUri && isPlaying===false) && <p>Playing...</p>}
     <SpotifyPlayer 
         token={accessToken}
         showSaveIcon
         callback={state => {
-          if (!state.isPlaying) setPlay(false)
+          if (!state.isPlaying){
+            setPlay(false)
+          } else
+            setIsPlaying(true)
         }}
         styles={{
           activeColor: '#fff',
