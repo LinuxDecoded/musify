95cb2ca4776206cce199236e2f75e7b2c98803bd
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-07-03 14:05:46 +0530

    refactored spotify web api object to different fil

---
+++ src/components/spotifyApi.js
@@ -0,0 +1,14 @@
+import SpotifyWebApi from "spotify-web-api-node";
+import  SpotifyWebApiServer from 'spotify-web-api-node/src/server-methods';
+
+SpotifyWebApi._addMethods = function(fncs) {
+  Object.assign(this.prototype, fncs);
+};
+SpotifyWebApi._addMethods(SpotifyWebApiServer);
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: import.meta.env.VITE_CLIENT_ID,
+    redirectUri: import.meta.env.VITE_REDIRECT_URI,
+});
+
+export default spotifyApi;
